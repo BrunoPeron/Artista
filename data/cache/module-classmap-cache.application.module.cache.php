00cb3719b2d6bc2504f21ffc0c7c6d1fd05ff2d7
bruno <bruno.peron@unochapeco.edu.br>
2020-06-09 09:59:51 -0300

    correcao apis extras da master

---
+++ data/cache/module-classmap-cache.application.module.cache.php
@@ -0,0 +1,8 @@
+<?php
+return array (
+  'User\\Module' => '/home/bruno/PhpstormProjects/skeleton/module/User/Module.php',
+  'Core\\Module' => '/home/bruno/PhpstormProjects/skeleton/module/Core/Module.php',
+  'Sistema\\Module' => '/home/bruno/PhpstormProjects/skeleton/module/Sistema/Module.php',
+  'Usuarios\\Module' => '/home/bruno/PhpstormProjects/skeleton/module/Usuarios/Module.php',
+  'Projeto\\Module' => '/home/bruno/PhpstormProjects/skeleton/module/Projeto/Module.php',
+);
--- module/Usuarios/src/V1/Rest/Usuario/UsuarioCollection.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Usuarios\V1\Rest\Usuario;
-
-use Zend\Paginator\Paginator;
-
-class UsuarioCollection extends Paginator
-{
-}
